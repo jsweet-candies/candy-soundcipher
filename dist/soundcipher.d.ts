d2b75fece3344050a7fe6754689f3fac4379d58c
Louis Grignon <louis.grignon@gmail.com>
2018-03-04 05:13:31 +0100

    doc sound cipher candy bas

---
+++ dist/soundcipher.d.ts
@@ -0,0 +1,14 @@
+/* Generated from Java with JSweet 2.0.0-SNAPSHOT - http://www.jsweet.org */
+declare namespace arb.soundcipher {
+    /**
+     * An example of a library definition.
+     * @param {*} o
+     * @class
+     */
+    export class SoundCipher {
+        public constructor(o : any);
+
+        public static playNote(pitch : number, dynamic : number, duration : number);
+    }
+}
+
---
+++ src/main/java/def/soundcipherlib/arb/soundcipher/SoundCipher.java
@@ -0,0 +1,11 @@
+package def.soundcipherlib.arb.soundcipher;
+
+/**
+ * An example of a library definition.
+ */
+public class SoundCipher {
+	public SoundCipher(Object o) {
+	}
+
+	native public static void playNote(double pitch, double dynamic, double duration);
+}
