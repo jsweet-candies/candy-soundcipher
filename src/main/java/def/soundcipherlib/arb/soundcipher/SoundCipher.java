96246a2eeb4be283c625b3f3b2a8c81faf431507
Ovidiu Miu <ovidiu.m94@gmail.com>
2018-05-23 22:39:05 +0300

    added new instruments, added method for loading/changing instrument

--- src/main/java/def/soundcipherlib/arb/soundcipher/SoundCipher.java
+++ src/main/java/def/soundcipherlib/arb/soundcipher/SoundCipher.java
@@ -8,11 +8,24 @@ public class SoundCipher {
 	}
 
     public static final float PIANO = 0;
-    public static final float SYNTH_DRUM = 118;
+    public static final float XYLOPHONE=13;
     public static final float ELECTRIC_GUITAR = 27;
+    public static final float ACOUSTIC_BASS=32;
+    public static final float STRINGS=48;
+    public static final float ORCHESTRA_HIT=55;
+    public static final float TRUMPET=56;
+    public static final float TUBA=58;
+    public static final float BRASS=61;
+    public static final float ALTO_SAX=65;
+    public static final float CLARINET=71;
+    public static final float FLUTE=73;
+    public static final float TAIKO=116;
+    public static final float SYNTH_DRUM = 118;
 
     public double instrument;
 
 	native public void playNote(double note, double dynamic, double duration);
 	native public void playChord(double[] notes, double dynamic, double duration);
+	native public void instrument(double instrumentCode);
+	native public static void loadInstrument(double instrumentCode);
 }
--- src/main/resources/META-INF/resources/webjars/soundcipher/0.0.10-beta-SNAPSHOT/index.js
+++ src/main/resources/META-INF/resources/webjars/soundcipher/0.0.10-beta-SNAPSHOT/index.js
@@ -14,20 +14,33 @@ var arb;
         let initialized = Promise.resolve();
         const DEFAULT_NOTE_DELAY = 0;
         const DEFAULT_NOTE_VELOCITY = 127;
+
         class SoundCipher {
-            static getInstrumentName(soundCipherInstrumentCode) {
-                switch (soundCipherInstrumentCode) {
-                    case SoundCipher.SYNTH_DRUM:
-                        return "synth_drum";
-                    case SoundCipher.ELECTRIC_GUITAR:
-                        return "electric_guitar_clean";
-                    default:
-                        return "acoustic_grand_piano";
-                }
+
+	constructor() {
+		// each SoundCipher instance will have a channel number
+		// so that multiple sounds can be played at the same time
+		if (SoundCipher.count >= 0) {
+			SoundCipher.count++;
+		} else {
+			SoundCipher.count = 0;
+		}
+		this.channel = SoundCipher.count;
+	}
+
+          static getInstrumentName(soundCipherInstrumentCode) {
+            if (this.instrumentSoundMap.get(soundCipherInstrumentCode)) {
+              return this.instrumentSoundMap.get(soundCipherInstrumentCode);
+            } else {
+              // default
+              return this.instrumentSoundMap.get(SoundCipher.PIANO);
             }
+          }
+
             get instrument() {
                 return this._instrument;
             }
+
             set instrument(instrumentCode) {
                 this._instrument = instrumentCode;
                 initialized = initialized.then(() => {
@@ -38,51 +51,109 @@ var arb;
                     });
                 });
             }
+
             playNote(note, dynamic = DEFAULT_NOTE_VELOCITY, duration = 0.75) {
                 return __awaiter(this, void 0, void 0, function* () {
                     log(`play note ${note} dynamic=${dynamic} duration=${duration} - WAITING`);
                     yield initialized;
                     log('ready to play');
-                    MIDI.setVolume(0, 127);
-                    MIDI.noteOn(0, note, DEFAULT_NOTE_VELOCITY, DEFAULT_NOTE_DELAY);
-                    MIDI.noteOff(0, note, DEFAULT_NOTE_DELAY + duration);
+                    MIDI.setVolume(this.channel, 127);
+                    MIDI.noteOn(this.channel, note, DEFAULT_NOTE_VELOCITY, DEFAULT_NOTE_DELAY);
+                    MIDI.noteOff(this.channel, note, DEFAULT_NOTE_DELAY + duration);
                 });
             }
+
             playChord(notes, dynamic = DEFAULT_NOTE_VELOCITY, duration = 0.75) {
                 return __awaiter(this, void 0, void 0, function* () {
                     log(`play chord [${notes.join(',')}] dynamic=${dynamic} duration=${duration} - WAITING`);
                     yield initialized;
                     log('ready to play chord');
-                    MIDI.setVolume(0, 127);
-                    MIDI.chordOn(0, notes, dynamic, DEFAULT_NOTE_DELAY);
-                    MIDI.chordOff(0, notes, DEFAULT_NOTE_DELAY + duration);
+                    MIDI.setVolume(this.channel, 127);
+                    MIDI.chordOn(this.channel, notes, dynamic, DEFAULT_NOTE_DELAY);
+                    MIDI.chordOff(this.channel, notes, DEFAULT_NOTE_DELAY + duration);
                 });
             }
-            static getLoadInstrumentArgs(instrumentName, onSuccess) {
+
+		static loadInstrument(instrumentCode) {
+		  return new Promise((resolve, reject) => {
+		var instrumentName = SoundCipher.getInstrumentName(instrumentCode);
+		log(`requesting 22 instrument ${instrumentName} soundfontUrl: ${SoundFontUrl}`);
+		MIDI.loadResource(SoundCipher.getLoadInstrumentArgs(instrumentName, resolve));
+	      });
+		}
+
+		instrument(instrumentCode) {
+		MIDI.programChange(this.channel, instrumentCode);
+            }
+
+            static getLoadInstrumentArgs(instrumentNames, onSuccess) {
                 return {
                     soundfontUrl: SoundFontUrl,
-                    instrument: instrumentName,
+                    instrument: instrumentNames,
                     onprogress: function (state, progress) {
-                        log(`instrument [${instrumentName}] loading - state=${state} progress=${progress}`);
+                        log(`instrument [${instrumentNames}] loading - state=${state} progress=${progress}`);
                     },
                     onsuccess: function () {
-                        const instrumentCode = MIDI.GM.byName[instrumentName].number;
-                        log(`instrument [${instrumentName}] loaded code=${instrumentCode}`);
-                        MIDI.programChange(0, instrumentCode);
+			for (var i=0; i<instrumentNames.length; i++) {
+				const instrumentCode = MIDI.GM.byName[instrumentNames[i]].number;
+				var instrumentName = instrumentNames[i];
+				log(`instrument [${instrumentName}] loaded code=${instrumentCode}`);
+			}
                         onSuccess();
                     }
                 };
             }
         }
         SoundCipher.PIANO = 0;
-        SoundCipher.SYNTH_DRUM = 118;
+        SoundCipher.XYLOPHONE=13;
         SoundCipher.ELECTRIC_GUITAR = 27;
+        SoundCipher.ACOUSTIC_BASS=32;
+        SoundCipher.STRINGS=48;
+        SoundCipher.ORCHESTRA_HIT=55;
+        SoundCipher.TRUMPET=56;
+        SoundCipher.TUBA=58;
+        SoundCipher.BRASS=61;
+        SoundCipher.ALTO_SAX=65;
+        SoundCipher.CLARINET=71;
+        SoundCipher.FLUTE=73;
+        SoundCipher.TAIKO=116;
+        SoundCipher.SYNTH_DRUM = 118;
+
+	SoundCipher.instrumentSoundMap = new Map();
+
+	SoundCipher.instrumentSoundMap.set(SoundCipher.PIANO, "acoustic_grand_piano");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.XYLOPHONE, "xylophone");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.ELECTRIC_GUITAR, "electric_guitar_clean");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.ACOUSTIC_BASS, "acoustic_bass");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.STRINGS, "string_ensemble_1");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.ORCHESTRA_HIT, "orchestra_hit");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.TRUMPET, "trumpet");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.TUBA, "tuba");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.BRASS, "brass_section");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.ALTO_SAX, "alto_sax");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.CLARINET, "clarinet");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.FLUTE, "flute");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.TAIKO, "taiko_drum");
+	SoundCipher.instrumentSoundMap.set(SoundCipher.SYNTH_DRUM, "synth_drum");
+
         soundcipher.SoundCipher = SoundCipher;
         if (!window['arb.soundcipher.SoundCipher__MIDIInitialized']) {
             window['arb.soundcipher.SoundCipher__MIDIInitialized'] = true;
             initialized = new Promise((resolve, reject) => {
-                const defaultInstrumentName = SoundCipher.getInstrumentName(SoundCipher.PIANO);
-                const initialize = () => MIDI.loadPlugin(SoundCipher.getLoadInstrumentArgs(defaultInstrumentName, resolve));
+                const defaultInstrumentsName = [SoundCipher.getInstrumentName(SoundCipher.PIANO)
+		,SoundCipher.getInstrumentName(SoundCipher.XYLOPHONE)
+		,SoundCipher.getInstrumentName(SoundCipher.ELECTRIC_GUITAR)
+		,SoundCipher.getInstrumentName(SoundCipher.ACOUSTIC_BASS)
+		,SoundCipher.getInstrumentName(SoundCipher.STRINGS)
+		,SoundCipher.getInstrumentName(SoundCipher.ORCHESTRA_HIT)
+		,SoundCipher.getInstrumentName(SoundCipher.TRUMPET)
+		,SoundCipher.getInstrumentName(SoundCipher.TUBA)
+		,SoundCipher.getInstrumentName(SoundCipher.BRASS)
+		,SoundCipher.getInstrumentName(SoundCipher.ALTO_SAX)
+		,SoundCipher.getInstrumentName(SoundCipher.FLUTE)
+		,SoundCipher.getInstrumentName(SoundCipher.TAIKO)
+		,SoundCipher.getInstrumentName(SoundCipher.SYNTH_DRUM)];
+                const initialize = () => MIDI.loadPlugin(SoundCipher.getLoadInstrumentArgs(defaultInstrumentsName, resolve));
                 log('request SoundCipher initialization');
                 if (document.readyState == 'complete') {
                     initialize();
@@ -91,6 +162,8 @@ var arb;
                     window.onload = initialize;
                 }
             });
+						console.log("abc");
+						console.log(initialized);
         }
         function log(message) {
             console.info(`SoundCipher: ${message}`);
