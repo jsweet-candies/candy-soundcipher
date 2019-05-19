be785c8f6e2fc333ee88678d0cf1b296db0fe576
Chi Bong Ho <chibong@ktbyte.com>
2019-05-19 19:29:04 -0400

    add javadocs

--- dist/soundcipher.d.ts
+++ dist/soundcipher.d.ts
@@ -1,8 +1,12 @@
 /* Generated from Java with JSweet 2.0.0-SNAPSHOT - http://www.jsweet.org */
 declare namespace arb.soundcipher {
     /**
-     * An example of a library definition.
-     * @param {*} o
+     * Constructor to initialize the SoundCipher object with the Processing context
+     * it is running in. In Processing, The SoundCipher object should be initialized as follows:
+     * <pre>
+     * SoundCipher sc = SoundCipher(this);
+     * </pre>
+     * @param {*} o    The Processing context the SoundCipher object will run in.
      * @class
      */
     export class SoundCipher {
@@ -38,12 +42,37 @@ declare namespace arb.soundcipher {
 
         public instrument : number;
 
+        /**
+         * Plays a note with the given pitch, dynamic (loudness) and duration.
+         * @param {number} note    The pitch of the note. This is a MIDI note, with the middle C
+         * being 60, and increases by 1 for each half step.
+         * @param {number} dynamic   The loudness of the note, from 0 to 127.
+         * @param {number} duration   The duration of the note in seconds.
+         */
         public playNote(note : number, dynamic : number, duration : number);
 
+        /**
+         * Plays an array of notes (simultaneously) with the given pitches , dynamic (loudness) and duration.
+         * @param {Array} notes    The pitches of the notes. These are MIDI notes, with the middle C
+         * being 60, and increases by 1 for each half step.
+         * @param {number} dynamic   The loudness of the note, from 0 to 127.
+         * @param {number} duration   The duration of the note in seconds.
+         */
         public playChord(notes : number[], dynamic : number, duration : number);
 
+        /**
+         * Changes the instrument played by the current SoundCipher object. Currently,
+         * only constants defined in this class are supported.
+         * @param {number} instrumentCode    The instrument to load
+         */
         public changeInstrument(instrumentCode : number);
 
+        /**
+         * Preload the sound file specified by the instrumentCode. This function can be called
+         * before the <code>playNote</code> or <code>playChord</code> to prevent latency
+         * on the first play caused by having to load the sound file.
+         * @param {number} instrumentCode    The instrument to load
+         */
         public static loadInstrument(instrumentCode : number);
 
         public static getInstrumentName(instrumentCode : number) : string;
--- src/main/java/def/soundcipherlib/arb/soundcipher/SoundCipher.java
+++ src/main/java/def/soundcipherlib/arb/soundcipher/SoundCipher.java
@@ -1,11 +1,48 @@
 package def.soundcipherlib.arb.soundcipher;
 
 /**
- * An example of a library definition.
+ * SoundCipher is a library that you can use to play notes and make music in Processing.
+ * The SoundCipher object should be initialized as follows:
+ * <pre>
+ * SoundCipher sc = SoundCipher(this);
+ * </pre>
+ * To play a note:
+ * <pre>
+ * sc.playNote(60,100,2.0);
+ * </pre>
+ * To play a chord:
+ * <pre>
+ * sc.playChord(new double[]{60, 64, 67},100,2.0);
+ * </pre>
+ * To play a short tune:
+ * <pre>
+ * 
+ * SoundCipher sc = new SoundCipher(this); 
+ * int[] notes = {60,60,67,67,69,69,67}; 
+ * 
+ * int frames_per_note = 30; 
+ * int noteIndex = 0;
+ * 
+ * void draw(){ 
+ *   if(frameCount % frames_per_note == 1 && noteIndex < notes.length) {
+ *     sc.playNote(notes[noteIndex], 100, 0.2);
+ *     noteIndex++;
+ *   }
+ * }
+ * </pre>
  */
 public class SoundCipher {
-	public SoundCipher(Object o) {
-	}
+
+    /**
+     * Constructor to initialize the SoundCipher object with the Processing context
+     * it is running in. In Processing, The SoundCipher object should be initialized as follows:
+     * <pre>
+     * SoundCipher sc = SoundCipher(this);
+     * </pre>
+     * @param o    The Processing context the SoundCipher object will run in.
+     */
+    public SoundCipher(Object o) {
+    }
 
     public static final float PIANO = 0;
     public static final float XYLOPHONE=13;
@@ -24,9 +61,38 @@ public class SoundCipher {
 
     public double instrument;
 
-	native public void playNote(double note, double dynamic, double duration);
-	native public void playChord(double[] notes, double dynamic, double duration);
-	native public void changeInstrument(double instrumentCode);
-	native public static void loadInstrument(double instrumentCode);
+    /**
+     * Plays a note with the given pitch, dynamic (loudness) and duration.
+     * @param note    The pitch of the note. This is a MIDI note, with the middle C 
+     *                being 60, and increases by 1 for each half step.
+     * @param dynamic   The loudness of the note, from 0 to 127.
+     * @param duration   The duration of the note in seconds.
+     */
+    native public void playNote(double note, double dynamic, double duration);
+
+
+    /**
+     * Plays an array of notes (simultaneously) with the given pitches , dynamic (loudness) and duration.
+     * @param notes    The pitches of the notes. These are MIDI notes, with the middle C 
+     *                being 60, and increases by 1 for each half step.
+     * @param dynamic   The loudness of the note, from 0 to 127.
+     * @param duration   The duration of the note in seconds.
+     */
+    native public void playChord(double[] notes, double dynamic, double duration);
+
+    /**
+     * Changes the instrument played by the current SoundCipher object. Currently,
+     * only constants defined in this class are supported.
+     * @param instrumentCode    The instrument to load
+     */
+    native public void changeInstrument(double instrumentCode);
+
+    /**
+     * Preload the sound file specified by the instrumentCode. This function can be called
+     * before the <code>playNote</code> or <code>playChord</code> to prevent latency 
+     * on the first play caused by having to load the sound file.
+     * @param instrumentCode    The instrument to load
+     */
+    native public static void loadInstrument(double instrumentCode);
     native public static String getInstrumentName(double instrumentCode);
 }
